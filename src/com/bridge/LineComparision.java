6105688c99309c48e92b0c481ec0c79a660aea93
JAISHRI <sjayashree529@gmail.com>
2022-03-10 20:59:28 +0530

    WEL COME

---
+++ src/com/bridge/LineComparision.java
@@ -0,0 +1,11 @@
+package com.bridge;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wel Come To Line Comparision Computation");
+
+	}
+
+}
